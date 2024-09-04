4b70695058b0ff304538f30d11c371aa74bd6ee1
404 <sametalpdeger9@gmail.com>
2024-09-04 19:53:06 +0300

    Initial commit

---
+++ src/utils/toPusherKey.ts
@@ -0,0 +1,5 @@
+/**
+ * Converts colons to double underscores because Pusher doesn't allow colons in channel names.
+ * We are using colons in our channel names to separate names.
+ */
+export const toPusherKey = (key: string) => key.replace(/:/g, '--')
